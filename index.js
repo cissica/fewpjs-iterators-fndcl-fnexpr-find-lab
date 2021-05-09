a09ea22f2a1502f2e2ff53e4d41313858d8cc893
cissica <cecemartinez@hotmail.com>
2021-05-09 10:36:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const record = [
-    {year: "2018", result: "L"},
-    {year: "2017", result: "W"},
-    {year: "2016", result: "N/A"}
 
+function superbowlWin(record) {
+    let result = record.find( ({ result }) => result === 'W' );
+    if(result === undefined){
+        return undefined
+    } else {
+        return result.year
+    }
+}
 
